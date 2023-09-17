b16da4e329ad28dc4c3ce3c503710d59b0f3cf5d
Lucas Galdino Sartorato <122971636+lucassgaldino@users.noreply.github.com>
2023-09-17 16:16:14 +0100

    Add files via upload

---
+++ form.js
@@ -0,0 +1,31 @@
+import { server } from "./server.js"
+
+const form = document.querySelector("#form")
+const input = document.querySelector("#url")
+const content = document.querySelector("#content")
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault()
+
+  const videoURL = input.value
+
+  if (!videoURL.includes("shorts")) {
+    return (content.textContent = "Esse vídeo não é um short")
+  }
+
+  const [_, params] = videoURL.split("/shorts/")
+  const [videoId] = params.split("?si")
+
+  content.textContent = "Obtendo o texto do audio..."
+
+  const transcription = await server.get("/summary/" + videoId)
+
+  content.textContent = "Realizando o resumo..."
+
+  const summary = await server.post("/summary", {
+    text: transcription.data.result,
+  })
+
+  content.textContent = transcription.data.result
+  content.classList.remove("placeholder")
+})
